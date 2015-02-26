579c3e4cbc857bafd4a75f3d3a4a4dd002676803
tophat8855 <hrfarley@gmail.com>
2015-02-26 14:09:35 -0800

    blue pins

--- app/routes/hashtags.js
+++ app/routes/hashtags.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function  () {
-    return this.store.find("hashtag")
+    return this.store.find("hashtag");
   },
   activate: function () {
-    var hashtags = this.modelFor(this.routeName)
+    var hashtags = this.modelFor(this.routeName);
 
     // tweets.forEach(function (tweet) {
     //   console.log(tweet.get("lat"));
@@ -15,23 +15,23 @@ export default Ember.Route.extend({
     //     console.log(hashtag.get("text"));
     //   })
     // })
-    var pinInfo = []
+    var pinInfo = [];
     var geojson = {
       "type": "FeatureCollection",
       "features": []
-    }
+    };
 
     hashtags.forEach(function (hashtag) {
-      console.log(hashtag.get("tweets"))
+      //console.log(hashtag.get("tweets"));
 
-      var tweets = hashtag.get("tweets")
+      var tweets = hashtag.get("tweets");
 
       tweets.forEach(function  (tweet) {
-        var lat = tweet.get("lat")
-        var lng = tweet.get("lng")
+        var lat = tweet.get("lat");
+        var lng = tweet.get("lng");
         var coords = [lng, lat];
-        var title = "#" + hashtag.get("text")
-        
+        var title = "#" + hashtag.get("text");
+
         pinInfo.push(
           {
             "type": "Feature",
@@ -42,21 +42,28 @@ export default Ember.Route.extend({
             "properties": {
               "title": title + "<hr>",
               "description": tweet.get("text") + "<hr> by " + tweet.get("username"),
+              "icon": {
+                "iconUrl": "assets/images/Twitter_logo_blue_small.png",
+                "iconSize": [20, 20],
+                "iconAnchor": [10, 10],
+                "popupAnchor": [0, -10],
+                "className": "dot"
+              }
             }
           }
         );
 
-      })
-    })
+      });
+    });
 
-    geojson.features = pinInfo
+    geojson.features = pinInfo;
 
     setTimeout(() =>{
       L.mapbox.accessToken = 'pk.eyJ1IjoicG9zdGF6dXJlIiwiYSI6IkJYZVBuSjgifQ.YPwaTygKiks84wDu8DuejA';
-      var map = L.mapbox.map('map', 'laurenjrichie.ladp904d').setView([37.7713,-122.439], 13); // can use .setView here to specify coordinates
+      var map = L.mapbox.map('map', 'postazure.lalplid').setView([37.7713,-122.439], 13); // can use .setView here to specify coordinates
 
       var myLayer = L.mapbox.featureLayer().addTo(map);
       myLayer.setGeoJSON(geojson);
-    })
+    });
   }
 });
--- app/routes/tweets.js
+++ app/routes/tweets.js
@@ -2,16 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function  () {
-    return this.store.find("tweet")
+    return this.store.find("tweet");
   },
   activate: function () {
-    var tweets = this.modelFor(this.routeName)
-    
-    var pinInfo = []
+    var tweets = this.modelFor(this.routeName);
+
+    var pinInfo = [];
     var geojson = {
       "type": "FeatureCollection",
       "features": []
-    }
+    };
 
     tweets.forEach(function (tweet) {
 
@@ -27,20 +27,29 @@ export default Ember.Route.extend({
           "properties": {
             "title": tweet.get("username") + "<hr>",
             "description": tweet.get("text"),
+            //"marker-symbol": 'cafe',
+            'marker-size': 'small',
+            'marker-color': '#4099ff'
+            // "icon": {
+            //   "iconUrl": "https://www.mapbox.com/maki/renders/airport-24@2x.png",
+            //   "iconSize": [24, 24],
+
+            //}
           }
         }
       );
 
-    })
+    });
 
-    geojson.features = pinInfo
+    geojson.features = pinInfo;
 
     setTimeout(() =>{
       L.mapbox.accessToken = 'pk.eyJ1IjoicG9zdGF6dXJlIiwiYSI6IkJYZVBuSjgifQ.YPwaTygKiks84wDu8DuejA';
-      var map = L.mapbox.map('map', 'postazure.lalplidi').setView([37.7713,-122.439], 13); // can use .setView here to specify coordinates
+      var map = L.mapbox.map('map', 'postazure.lalplidi').setView([37.7713,-122.439], 13);
 
       var myLayer = L.mapbox.featureLayer().addTo(map);
       myLayer.setGeoJSON(geojson);
-    })
+
+    });
   }
 });
