2eedb6ffb6078dec885ebccb8f81d2662c01b516
tophat8855 <hrfarley@gmail.com>
2015-02-26 10:17:09 -0800

    pins work from database

--- app/routes/tweets.js
+++ app/routes/tweets.js
@@ -5,23 +5,46 @@ export default Ember.Route.extend({
     return this.store.find("tweet")
   },
   activate: function () {
-    setTimeout(function() {
+    var tweets = this.modelFor(this.routeName)
+
+    // tweets.forEach(function (tweet) {
+    //   console.log(tweet.get("lat"));
+    //   console.log(tweet.get("lng"));
+    //   console.log(tweet.get("text"));
+    //   tweet.get("hashtags").forEach(function (hashtag) {
+    //     console.log(hashtag.get("text"));
+    //   })
+    // })
+    var pinInfo = []
+    var geojson = {
+      "type": "FeatureCollection",
+      "features": []
+    }
+
+    tweets.forEach(function (tweet) {
+
+      var coords = [tweet.get("lng"), tweet.get("lat")];
+
+      pinInfo.push(
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": coords
+          },
+          "properties": {
+            "title": tweet.get("text")
+          }
+        }
+      );
+
+    })
+
+    geojson.features = pinInfo
+
+    setTimeout(() =>{
       L.mapbox.accessToken = 'pk.eyJ1IjoicG9zdGF6dXJlIiwiYSI6IkJYZVBuSjgifQ.YPwaTygKiks84wDu8DuejA';
       var map = L.mapbox.map('map', 'laurenjrichie.ladp904d').setView([37.7713,-122.439], 13); // can use .setView here to specify coordinates
-
-      var geojson = [{
-                      "type": "FeatureCollection",
-                      "features": [
-                        {
-                          "type": "Feature",
-                          "geometry": {
-                            "type": "Point",
-                            "coordinates": [-122.388078331989, 37.7667867792956]
-                          },
-                          "properties": {}
-                        }
-                      ]
-                    }];
 
       var myLayer = L.mapbox.featureLayer().addTo(map);
       myLayer.setGeoJSON(geojson);
