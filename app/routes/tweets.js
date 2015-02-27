09de56afd6b365da5c3bceecd0e49ac5aa81b1c3
Max Gordon <gordonmaxc@gmail.com>
2015-02-27 11:24:20 -0800

    panToPin fixed

--- app/routes/tweets.js
+++ app/routes/tweets.js
@@ -15,42 +15,6 @@ export default Ember.Route.extend({
   },
 
   actions: {
-    panToPin: function  (tweet) {
-      return;
-      var map = this.map;
-      var lat = tweet.get("lat");
-      var lng = tweet.get("lng");
-      var text = tweet.get("text")
-      
-      if (text) {
-      var linkURL = text.match(/http:\S{2,}/g)
-      var text = text.replace(linkURL,"<a target='_blank' href='"+ linkURL +"'>"+ linkURL +"</a>")
-      };
-      
-      var markerjson = {
-        "type": "FeatureCollection",
-        "features": [
-          {
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [lng, lat]
-            },
-            "properties": {
-              "title": tweet.get("username") + "<hr>",
-              "description": text + "<hr>" + tweet.get("createdAt"),
-              'marker-size': 'small',
-              'marker-color': '#0088cc'
-            }
-          }
-        ]
-      }
-      var newPinLayer = L.mapbox.featureLayer(markerjson)
-      
-      newPinLayer.addTo(map)
-      newPinLayer.openPopup();
-
-      map.setView([lat, lng], 14);
-    }
+    
   },
 });
--- app/views/tweets.js
+++ app/views/tweets.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
+  map: null,
   didInsertElement: function(){
     L.mapbox.accessToken = 'pk.eyJ1IjoicG9zdGF6dXJlIiwiYSI6IkJYZVBuSjgifQ.YPwaTygKiks84wDu8DuejA';
     var map = L.mapbox.map('map', 'postazure.lalplidi').setView([37.7713,-122.439], 13); // can use .setView here to specify coordinates
-
+    this.set("map", map)
     var myLayer = L.mapbox.featureLayer().addTo(map);
     this.set('myLayer', myLayer)
   },
@@ -28,7 +29,7 @@ export default Ember.View.extend({
       var text = tweet.get("text")
       
       if (text) {
-      var linkURL = text.match(/http:\S{2,}/g)
+      var linkURL = text.match(/http\S{2,}/g)
       var text = text.replace(linkURL,"<a target='_blank' href='"+ linkURL +"'>"+ linkURL +"</a>")
       };
 
@@ -54,5 +55,45 @@ export default Ember.View.extend({
 
     
     myLayer.setGeoJSON(geojson);
-  }.observes('controller.sortedTweetsDesc.length', 'myLayer')
+  }.observes('controller.sortedTweetsDesc.length', 'myLayer'),
+
+  actions: {
+    panToPin: function  (tweet) {
+
+      var map = this.map;
+      var lat = tweet.get("lat");
+      var lng = tweet.get("lng");
+      var text = tweet.get("text")
+      
+      if (text) {
+      var linkURL = text.match(/http\S{2,}/g)
+      var text = text.replace(linkURL,"<a target='_blank' href='"+ linkURL +"'>"+ linkURL +"</a>")
+      };
+      
+      var markerjson = {
+        "type": "FeatureCollection",
+        "features": [
+          {
+            "type": "Feature",
+            "geometry": {
+              "type": "Point",
+              "coordinates": [lng, lat]
+            },
+            "properties": {
+              "title": tweet.get("username") + "<hr>",
+              "description": text + "<hr>" + tweet.get("createdAt"),
+              'marker-size': 'small',
+              'marker-color': '#0088cc'
+            }
+          }
+        ]
+      }
+      var newPinLayer = L.mapbox.featureLayer(markerjson)
+      
+      newPinLayer.addTo(map)
+      newPinLayer.openPopup();
+
+      map.setView([lat, lng], 14);
+    }
+  }
 });
