6273f30968814175ffa8049828e00051736efb07
tophat8855 <hrfarley@gmail.com>
2015-02-27 10:37:27 -0800

    trying to get it to recognize L.MarcherClusterGroup

--- app/routes/tweets.js
+++ app/routes/tweets.js
@@ -10,13 +10,13 @@ export default Ember.Route.extend({
       var map = this.map;
       var lat = tweet.get("lat");
       var lng = tweet.get("lng");
-      var text = tweet.get("text")
-      
+      var text = tweet.get("text");
+
       if (text) {
-      var linkURL = text.match(/http:\S{2,}/g)
-      var text = text.replace(linkURL,"<a target='_blank' href='"+ linkURL +"'>"+ linkURL +"</a>")
-      };
-      
+      var linkURL = text.match(/http:\S{2,}/g);
+      var text = text.replace(linkURL,"<a target='_blank' href='"+ linkURL +"'>"+ linkURL +"</a>");
+      }
+
       var markerjson = {
         "type": "FeatureCollection",
         "features": [
@@ -34,10 +34,10 @@ export default Ember.Route.extend({
             }
           }
         ]
-      }
-      var newPinLayer = L.mapbox.featureLayer(markerjson)
-      
-      newPinLayer.addTo(map)
+      };
+      var newPinLayer = L.mapbox.featureLayer(markerjson);
+
+      newPinLayer.addTo(map);
       newPinLayer.openPopup();
 
       map.setView([lat, lng], 14);
@@ -56,12 +56,12 @@ export default Ember.Route.extend({
     tweets.forEach(function (tweet) {
 
       var coords = [tweet.get("lng"), tweet.get("lat")];
-      var text = tweet.get("text")
-      
+      var text = tweet.get("text");
+
       if (text) {
-      var linkURL = text.match(/http:\S{2,}/g)
-      var text = text.replace(linkURL,"<a target='_blank' href='"+ linkURL +"'>"+ linkURL +"</a>")
-      };
+      var linkURL = text.match(/http:\S{2,}/g);
+      var text = text.replace(linkURL,"<a target='_blank' href='"+ linkURL +"'>"+ linkURL +"</a>");
+      }
 
       pinInfo.push(
         {
@@ -90,31 +90,15 @@ export default Ember.Route.extend({
         .setView([37.7713,-122.439], 13) // can use .setView here to specify coordinates
         .addLayer(L.mapbox.tileLayer('postazure.lalplidi'));
 
-      this.set("map", map)
+      this.set("map", map);
 
-      var myLayer = L.mapbox.featureLayer().addTo(map);
-      myLayer.setGeoJSON(geojson);
+      // var myLayer = L.mapbox.featureLayer().addTo(map);
+      // myLayer.setGeoJSON(geojson);
 
+      var markers = new L.MarkerClusterGroup();
+      markers.addLayer(geojson);
+      markers.addTo(map);
 
-      // var markers = new L.MarkerClusterGroup();
-      // markers.addLayer(new L.Marker(getRandomLatLng(map)));
-      // // ... Add more layers ...
-      // map.addLayer(markers);
-
-
-      // var myLayer = L.mapbox.featureLayer(geojson);
-      // var markers = new L.MarkerClusterGroup();
-      // var clusterGroup = new L.MarkerClusterGroup();
-      // myLayer.eachLayer(function(layer) {
-      
-
-      //     //clusterGroup.addLayer(layer);
-      // });
-
-      // // map.addLayer(clusterGroup);
-      // clusterGroup.addTo(map)
-
-      // myLayer.addTo(map)
-    })
+    });
   }
 });
